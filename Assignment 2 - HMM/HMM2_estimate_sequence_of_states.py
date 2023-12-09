2999ff5f83aec5c704413064a7f93ebf86fd8a93
Casper Kristiansson <casper.kristiansson@yahoo.se>
2023-12-09 13:24:55 +0100

    HMM2 no numpy

--- Assignment 2 - HMM/HMM2_estimate_sequence_of_states.py
+++ Assignment 2 - HMM/HMM2_estimate_sequence_of_states.py
@@ -10,70 +10,62 @@ Output
 You should output the most probable sequence of states as zero-based indices separated by spaces. Do not output the length of the sequence.
 
 Sample Input 1:
-4 4 0.0 0.8 0.1 0.1 0.1 0.0 0.8 0.1 0.1 0.1 0.0 0.8 0.8 0.1 0.1 0.0 
-4 4 0.9 0.1 0.0 0.0 0.0 0.9 0.1 0.0 0.0 0.0 0.9 0.1 0.1 0.0 0.0 0.9 
-1 4 1.0 0.0 0.0 0.0 
-4 1 1 2 2 
+4 4 0.0 0.8 0.1 0.1 0.1 0.0 0.8 0.1 0.1 0.1 0.0 0.8 0.8 0.1 0.1 0.0
+4 4 0.9 0.1 0.0 0.0 0.0 0.9 0.1 0.0 0.0 0.0 0.9 0.1 0.1 0.0 0.0 0.9
+1 4 1.0 0.0 0.0 0.0
+4 1 1 2 2
 
 Sample Output 1:
 0 1 2 1
 """
-import numpy as np
 import sys
-
-
-def read_matrix(matrix_raw):
-    elements = list(map(float, matrix_raw.split()))
-    rows, cols = int(elements[0]), int(elements[1])
-    matrix = np.array(elements[2:]).reshape(rows, cols)
-
-    return matrix
+import tools
 
 
 def main(input_data):
     input_data = input_data.split("\n")
 
-    transition_matrix = read_matrix(input_data[0])
-    number_of_states = transition_matrix.shape[0]
-
-    emission_matrix = read_matrix(input_data[1])
-    initial_state_matrix = read_matrix(input_data[2])
-
+    transition_matrix = tools.read_matrix(input_data[0])
+    emission_matrix = tools.read_matrix(input_data[1])
+    initial_state_matrix = tools.read_matrix(input_data[2])
     emission_sequence = list(map(int, input_data[3].split()))
-    number_of_emissions = emission_sequence[0]
-    emission_sequence = np.array(emission_sequence[1:])
 
-    delta = np.zeros((number_of_emissions, number_of_states))
-    path = np.zeros((number_of_emissions, number_of_states), dtype=int)
+    number_of_states = len(initial_state_matrix[0])
+    number_of_emissions = emission_sequence[0]
+
+    emission_sequence = emission_sequence[1:]
+
+    delta = tools.matrix_initialization(number_of_emissions, number_of_states)
+    path = tools.matrix_initialization(number_of_emissions, number_of_states)
 
     # Initialization step
-    delta[0, :] = initial_state_matrix * emission_matrix[:, emission_sequence[0]]
+    for j in range(number_of_states):
+        delta[0][j] = initial_state_matrix[0][j] * emission_matrix[j][emission_sequence[0]]
 
     # Recursion step
     for t in range(1, number_of_emissions):
         for j in range(number_of_states):
-            prob = delta[t - 1] * transition_matrix[:, j] * emission_matrix[j, emission_sequence[t]]
-            delta[t, j] = np.max(prob)
-            path[t, j] = np.argmax(prob)
+            prob = [delta[t - 1][i] * transition_matrix[i][j] * emission_matrix[j][emission_sequence[t]] for i in range(number_of_states)]
+            delta[t][j] = max(prob)
+            path[t][j] = prob.index(max(prob))
 
     # Termination and path-backtracking
-    last_state = np.argmax(delta[-1, :])
-    number_of_steps = path.shape[0]
+    last_state = delta[number_of_emissions - 1].index(max(delta[number_of_emissions - 1]))
 
     best_path = [last_state]
-    for t in range(number_of_steps - 1, 0, -1):
-        last_state = path[t, last_state]
+    for t in range(number_of_emissions - 1, 0, -1):
+        last_state = path[t][last_state]
         best_path.insert(0, last_state)
 
     return " ".join(map(str, best_path))
 
 
 if __name__ == "__main__":
-    if True:
-        input_data = """4 4 0.0 0.8 0.1 0.1 0.1 0.0 0.8 0.1 0.1 0.1 0.0 0.8 0.8 0.1 0.1 0.0 
-4 4 0.9 0.1 0.0 0.0 0.0 0.9 0.1 0.0 0.0 0.0 0.9 0.1 0.1 0.0 0.0 0.9 
-1 4 1.0 0.0 0.0 0.0 
-4 1 1 2 2 
+    if False:
+        input_data = """4 4 0.0 0.8 0.1 0.1 0.1 0.0 0.8 0.1 0.1 0.1 0.0 0.8 0.8 0.1 0.1 0.0
+4 4 0.9 0.1 0.0 0.0 0.0 0.9 0.1 0.0 0.0 0.0 0.9 0.1 0.1 0.0 0.0 0.9
+1 4 1.0 0.0 0.0 0.0
+4 1 1 2 2
     """
     else:
         input_data = sys.stdin.read()
