348591f0e0d1cd820f44598a269eaf9a6c97288b
Casper Kristiansson <casper.kristiansson@yahoo.se>
2023-12-09 13:02:44 +0100

    HMM0 fix no numpy

--- Assignment 2 - HMM/HMM0_next_emission_distribution.py
+++ Assignment 2 - HMM/HMM0_next_emission_distribution.py
@@ -19,34 +19,29 @@ Sample Input 1:
 Sample Output 1:
 1 3 0.3 0.6 0.1
 """
-import numpy as np
 import sys
-
-
-def read_matrix(matrix_raw):
-    elements = list(map(float, matrix_raw.split()))
-    rows, cols = int(elements[0]), int(elements[1])
-    matrix = np.array(elements[2:]).reshape(rows, cols)
-
-    return matrix
+import tools
 
 
 def main(input_data):
     input_data = input_data.split("\n")
 
-    transition_matrix = read_matrix(input_data[0])
-    emission_matrix = read_matrix(input_data[1])
-    initial_state_matrix = read_matrix(input_data[2])
+    transition_matrix = tools.read_matrix(input_data[0])
+    emission_matrix = tools.read_matrix(input_data[1])
+    initial_state_matrix = tools.read_matrix(input_data[2])
 
-    emission_distribution = np.dot(initial_state_matrix, np.dot(transition_matrix, emission_matrix))
-    rows, cols = emission_distribution.shape
-    emission_distribution = np.round(emission_distribution, decimals=2)
+    emission_distribution = tools.matrix_multiply(initial_state_matrix, tools.matrix_multiply(transition_matrix, emission_matrix))
+    rows, cols = tools.matrix_dimensions(emission_distribution)
 
-    return f"{rows} {cols} " + " ".join(map(str, emission_distribution.flatten()))
+    emission_distribution = tools.matrix_round(emission_distribution)
+
+    flattened_list = [item for sublist in emission_distribution for item in sublist]
+
+    return f"{rows} {cols} " + " ".join(map(str, flattened_list))
 
 
 if __name__ == "__main__":
-    if False:
+    if True:
         input_data = """4 4 0.2 0.5 0.3 0.0 0.1 0.4 0.4 0.1 0.2 0.0 0.4 0.4 0.2 0.3 0.0 0.5
 4 3 1.0 0.0 0.0 0.0 1.0 0.0 0.0 0.0 1.0 0.2 0.6 0.2
 1 4 0.0 0.0 0.0 1.0
---
+++ Assignment 2 - HMM/tools.py
@@ -0,0 +1,26 @@
+def read_matrix(matrix_raw):
+    elements = list(map(float, matrix_raw.split()))
+    rows, cols = int(elements[0]), int(elements[1])
+    elements = elements[2:]
+    matrix = [elements[cols * i: cols * (i + 1)] for i in range(rows)]
+
+    return matrix
+
+
+def matrix_multiply(A, B):
+    result = [[0 for _ in range(len(B[0]))] for _ in range(len(A))]
+    for i in range(len(A)):
+        for j in range(len(B[0])):
+            for k in range(len(B)):
+                result[i][j] += A[i][k] * B[k][j]
+    return result
+
+
+def matrix_dimensions(matrix):
+    rows = len(matrix)
+    cols = len(matrix[0]) if rows > 0 else 0
+    return rows, cols
+
+
+def matrix_round(matrix, digits=2):
+    return [[round(value, digits) for value in row] for row in matrix]
